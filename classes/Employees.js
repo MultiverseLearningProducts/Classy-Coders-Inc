8c3551207b82c4f19b08d0d4e5774de7387ddc4d
Valeriia Harmash <valeriaharmash@gmail.com>
2023-07-27 15:53:40 -0400

    done

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,35 +1,48 @@
 class Employees {
 	#salary
 	#isHired = true
+	static #allEmployees = []
+
 	constructor(name, position, salary) {
-		this.name = name;
-		this.position = position;
-		this.#salary = salary;
+		this.name = name
+		this.position = position
+		this.#salary = salary
+		Employees.#allEmployees.push(this)
+	}
+
+	static getEmployees() {
+		return Employees.#allEmployees
 	}
 
 	getSalary() {
-		return this.#salary;
+		return this.#salary
+	}
+
+	static getTotalPayroll() {
+		let total = 0
+		for (let employee of Employees.#allEmployees) {
+			total += employee.getSalary()
+		}
+		return total
 	}
 
 	setSalary(amount) {
-		this.#salary = amount;
+		this.#salary = amount
 	}
 
 	getStatus() {
-		return this.#isHired;
+		return this.#isHired
 	}
 
 	setStatus(command) {
-		if (command === "hire") {
-			this.#isHired = true;
-		} else if (command === "fire") {
-			this.#isHired = false;
+		if (command === 'hire') {
+			this.#isHired = true
+		} else if (command === 'fire') {
+			this.#isHired = false
 		}
 	}
-
-    
 }
 
 module.exports = {
-    Employees,
+	Employees
 }
--- classes/SoftwareEngineer.js
+++ classes/SoftwareEngineer.js
@@ -1,10 +1,21 @@
-class SoftwareEngineer {
+const { Employees } = require('./Employees')
 
+class SoftwareEngineer extends Employees {
+	#programmingLanguages
+	constructor(name, position, salary, programmingLanguages) {
+		super(name, position, salary)
+		this.#programmingLanguages = programmingLanguages
+	}
 
+	setProgrammingLanguages(language) {
+		this.#programmingLanguages.push(language)
+	}
 
-    
+	getProgrammingLanguages() {
+		return this.#programmingLanguages
+	}
 }
 
 module.exports = {
-    SoftwareEngineer,
+	SoftwareEngineer
 }
