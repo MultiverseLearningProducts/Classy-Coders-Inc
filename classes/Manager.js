30cb4c977344c9a413b83940651c8a9aa05c0578
DavidGonzalez916 <dagonzalez@deloitte.com>
2024-03-13 15:33:12 -0400

    Added Manager Class

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,7 +1,17 @@
-class Manager {
+const {Employees} = require('./Employees')
+class Manager extends Employees {
+    #employeesManaged = [];
+    constructor(name, title, department){
+        super(name, title)
+        this.department = department;
+    }
 
-
-
+    getEmployeesManaged(){
+        return this.#employeesManaged;
+    }
+    setEmployeesManaged(employee){
+        this.#employeesManaged.push(employee);
+    }
     
 }
 
