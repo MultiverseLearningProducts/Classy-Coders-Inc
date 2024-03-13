c3cee36c7527961405b0d9c3187c89b56f4551c0
Jasmine Daniels <create.jasminedaniels@gmail.com>
2024-03-13 15:21:11 -0400

    Created Employee class

--- classes/Employees.js
+++ classes/Employees.js
@@ -1,7 +1,32 @@
 class Employees {
+    #salary;
+    #isHired = true;
 
+    constructor(name, title, salary){
+        this.name = name;
+        this.title = title;
+        this.#salary = salary;
+    }
 
+    getSalary(){
+        return this.#salary;
+    }
 
+    setSalary(amount){
+        this.#salary = amount;
+    }
+
+    getStatus(){
+        return this.#isHired;
+    }
+
+    setStatus(command){
+        if(command == 'fire'){
+            this.#isHired = false;
+        } else {
+            this.#isHired = true;
+        }
+    }
     
 }
 
