6a778129aa9d2254d4db4a89ef20e25ee4b189e2
Chris Horne <chrhorne@deloitte.com>
2024-03-13 12:47:33 -0700

    Added Employee class scaffolding.

--- index.js
+++ index.js
@@ -5,7 +5,41 @@ const { SoftwareEngineer } = require("./classes/SoftwareEngineer")
 
 // Run and debug your code here
 
+class Employees {
+    #salary;
+    #isHired;
+    static #allEmployees;
+    constructor(name, title, salary, isHired) {
+        this.name = name; 
+        this.title = title; 
+        this.#salary = salary;
+        this.#isHired = isHired;
 
+    }
+
+    setSalary(amount) {
+
+    }
+
+    status(command) {
+
+    }
+
+    getEmployees(){
+
+    }
+
+    setEmployees(Employees){
+
+    }
+
+    getTotalPayrool(employees){
+        
+    }
+
+
+
+}
 
 
 
