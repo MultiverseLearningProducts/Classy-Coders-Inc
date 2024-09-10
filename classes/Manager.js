db590b9668370fb27d67bfc23354417e5597ed19
Rtollinchi <rtollinchi23@gmail.com>
2024-09-10 17:02:36 -0400

    updated Manager.js

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,8 +1,18 @@
-class Manager {
+const Employees = require('./Employees')
 
-
-
-    
+class Manager extends Employees{
+    #employeesManaged
+    constructor(name, position, salary, department) {
+        super(name, position, salary, department)
+        this.department = department;
+        this.#employeesManaged = [];
+    }
+    getEmployessManaged() {
+        return this.#employeesManaged;
+    }
+    setEmployessManaged(newEmployee) {
+        this.#employeesManaged.push(newEmployee);
+    }
 }
 
 module.exports = {
