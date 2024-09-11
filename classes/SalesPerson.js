0204d589da9ad142574647ee14bcdb972de499f5
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2024-09-11 17:11:22 -0400

    1,2,3 done

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,10 +1,23 @@
-class SalesPerson {
+const { Employees } = require("./Employees");
 
-
-
-    
+class SalesPerson extends Employees {
+  #totalSales;
+  constructor(name, position, salary, clients) {
+    super(name, position, salary);
+    this.clients = clients;
+    this.#totalSales = 0;
+  }
+  getSalesNumber() {
+    return this.#totalSales;
+  }
+  makeSale(amount) {
+    // if (amount < 0) {
+    //   throw new Error("Sale amount cannot be negative");
+    // }
+    this.#totalSales += amount;
+  }
 }
 
 module.exports = {
-    SalesPerson,
-}
+  SalesPerson,
+};
