a303c70e4dc3db123bc753ac45a71a1e0a62fb64
Daniels <briana.daniels@hp.com>
2025-02-26 11:27:10 -0600

    Added Employee

--- classes/Employee.js
+++ classes/Employee.js
@@ -1,10 +1,29 @@
 class Employee {
-
-
-
-    
+#salary
+#isHired
+constructor (name, position) {
+    this.name = name;
+    this.position = position;
+    this.#salary = amount;
+    this.#isHired = hired;
 }
-
+getSalary(){
+    return this.#salary;
+}
+setSalary(amount){
+    this.#salary = amount;
+}
+getStatus(){
+    return this.#isHired;
+}
+setStatus(command){
+    if(command === 'hire'){
+        return true;
+    } else if 
+        (command === 'fire') {
+            return false;   } 
+}
+}
 module.exports = {
     Employee,
 }
